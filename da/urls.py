44438191ac713ca01504ef42b653bbdbe8ec1e1a
郝晶 <howdrink@Node-2.local>
2019-09-25 17:27:36 +0800

    增加后台管理接口

--- da/urls.py
+++ da/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import view
+from . import adminview
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('getqrcode/', view.getqrcode),
@@ -14,4 +15,15 @@ urlpatterns = [
     path('sendredpack/', view.sendredpack),
     path('scrapredpack/', view.scrapredpack),
     path('redpackrecorde/', view.redpackrecorde),
+    path('admingetusers/', adminview.admingetusers),
+    path('admingetuserinfobyid/', adminview.admingetuserinfobyid),
+    path('admingetuseraccountinfobyid/',
+         adminview.admingetuseraccountinfobyid),
+    path('admingettags/', adminview.admingettags),
+    path('admingetuseraccountinfobytagid/',
+         adminview.admingetuseraccountinfobytagid),
+    path('admingetredpacks/', adminview.admingetredpacks),
+    path('admingetredpackinfobyid/', adminview.admingetredpackinfobyid),
+    path('adminmodifytags/', adminview.adminmodifytags),
+    path('adminreset/', adminview.adminreset),
 ]
